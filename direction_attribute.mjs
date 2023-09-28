353ed2518d7641b63a6daad513ee04fe5b362dbc
Shohei Miyazaki <shohei_miyazaki@denkiyagi.jp>
2023-09-28 13:59:55 +0900

    テストを実行

--- direction_attribute.mjs
+++ direction_attribute.mjs
@@ -1,10 +1,19 @@
 import xpath from 'xpath'
 
+export function splitAttributeValue(value) {
+    const result = value.match(/_|[\w+-,.]+(\((\s|[\w+-,.])*\))?/g);
+    if (result) {
+        return result;
+    } else {
+        return [];
+    }
+}
+
 export function migrate(doc) {
-    const nodes = xpath.select("//Grid", doc)
+    const nodes = xpath.select("//Grid", doc);
     for (let i = 0; i < nodes.length; i++) {
-        const node = nodes[i]
-        node.setAttribute("hoge", "piyo")
-        node.removeAttribute("rows")
+        const node = nodes[i];
+        node.setAttribute("hoge", "piyo");
+        node.removeAttribute("rows");
     }
 }
---
+++ direction_attribute.test.js
@@ -0,0 +1,13 @@
+import * as direction_attribute from './direction_attribute.mjs';
+
+test.each([
+    {input: "", expected: []},
+    {input: "30", expected: ["30"]},
+    {input: "30 30 _ _", expected: ["30", "30", "_", "_"]},
+    {input: "dashed dashed dotted dashed", expected: ["dashed", "dashed", "dotted", "dashed"]},
+    {input: "Cmyk(1, 0, 0, 0)", expected: ["Cmyk(1, 0, 0, 0)"]},
+    {input: "cmyk(1, 0, 0, 0) cmyk(0, 1, 0, 0) cmyk(0, 0, 1, 0) cmyk(0, 0, 0, 1)", expected: ["cmyk(1, 0, 0, 0)", "cmyk(0, 1, 0, 0)", "cmyk(0, 0, 1, 0)", "cmyk(0, 0, 0, 1)"]},
+    {input: "${hide}", expected: ["${hide}"]},
+])('属性値を分解できる: $input', ({input, expected}) => {
+    expect(direction_attribute.splitAttributeValue(input)).toStrictEqual(expected);
+});
