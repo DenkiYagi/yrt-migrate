af1dbdeacfcafca9f3cff2119e7fbbfeebfa2efc
Shohei Miyazaki <shohei_miyazaki@denkiyagi.jp>
2023-09-29 16:22:47 +0900

    コマンドライン引数を実装

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import * as fs from 'fs/promises';
 import * as direction_attribute from './direction_attribute.mjs';
 import * as path from 'path';
 import * as msgpack from '@msgpack/msgpack';
+import * as util from 'util';
 
 function migrate(inputLayoutXml) {
     const doc = new DOMParser().parseFromString(inputLayoutXml, 'text/xml');
@@ -13,34 +14,111 @@ function migrate(inputLayoutXml) {
     return outputLayoutXml;
 }
 
-const fileName = process.argv[2];
-const ext = path.extname(fileName);
+function printHelp() {
+    console.log(`Usage: node index.js [options...] [input_file]
+    -i, --input <input_file>   入力ファイル名を指定します。このオプションを使用した場合は末尾のファイル名は省略できます
+    -o, --output <output_file> 出力ファイル名を指定します。省略した場合は入力ファイルを上書きします
+    -b, --backup <backpu_file> バックアップファイル名を指定します。省略した場合は {input_file}.old を使用します
+    -d, --dry-run              変換結果を表示します。ファイルへは出力されません
+    -h, --help                 このメッセージを表示します`);
+}
 
-if (ext == ".xml") {
-    await fs.copyFile(fileName, `${fileName}.old`);
-
-    const inputLayoutXml = await fs.readFile(fileName, 'utf-8');
-    const outputLayoutXml = migrate(inputLayoutXml);
-
-    fs.writeFile(fileName, outputLayoutXml);
-    process.exit(0);
-} else if (ext == ".yrt") {
-    await fs.copyFile(fileName, `${fileName}.old`);
-
-    const inputFile = await fs.readFile(fileName);
-    const yrtFile = msgpack.decode(inputFile);
-
-    const inputLayoutXml = yrtFile[0]
-    const outputLayoutXml = migrate(inputLayoutXml);
-
-    yrtFile[0] = outputLayoutXml;
-    const outputFile = msgpack.encode(yrtFile);
-
-    await fs.writeFile(fileName, Buffer.from(outputFile));
-    process.exit(0);
-} else {
-    console.error("非対応のファイル形式です")
+let args;
+try {
+    args = util.parseArgs({
+        options: {
+            "input": {
+                type: "string",
+                short: "i"
+            },
+            "output": {
+                type: "string",
+                short: "o"
+            },
+            "backup": {
+                type: "string",
+                short: "b"
+            },
+            "dry-run": {
+                type: "boolean",
+                short: "d"
+            },
+            "help": {
+                type: "boolean",
+                short: "h"
+            }
+        },
+        allowPositionals: true,
+    })
+} catch(error) {
+    console.error(error);
+    printHelp();
     process.exit(1);
 }
 
+if (args.values.help) {
+    printHelp();
+    process.exit(0);
+}
 
+let inputFileName;
+if (args.values.input) {
+    inputFileName = args.values.input;
+} else if (args.positionals.length >= 1) {
+    inputFileName = args.positionals[0];
+} else {
+    console.error("入力ファイルを指定してください");
+    process.exit(1);
+}
+
+const ext = path.extname(inputFileName);
+
+let backupFileName;
+if (args.values.backup) {
+    backupFileName = args.values.backup;
+} else {
+    backupFileName = `${inputFileName}.old`;
+}
+
+let outputFileName;
+if (args.values.output) {
+    outputFileName = args.values.output;
+} else {
+    outputFileName = inputFileName;
+}
+
+try {
+    if (ext == ".xml") {
+        const inputLayoutXml = await fs.readFile(inputFileName, 'utf-8');
+        const outputLayoutXml = migrate(inputLayoutXml);
+
+        if (args.values['dry-run']) {
+            console.log(outputLayoutXml);
+        } else {
+            await fs.copyFile(inputFileName, backupFileName);
+            await fs.writeFile(outputFileName, outputLayoutXml);
+        }
+    } else if (ext == ".yrt") {
+        const inputFile = await fs.readFile(inputFileName);
+        const yrtFile = msgpack.decode(inputFile);
+
+        const inputLayoutXml = yrtFile[0]
+        const outputLayoutXml = migrate(inputLayoutXml);
+
+        yrtFile[0] = outputLayoutXml;
+        const outputFile = msgpack.encode(yrtFile);
+
+        if (args.values['dry-run']) {
+            console.log(outputLayoutXml);
+        } else {
+            await fs.copyFile(inputFileName, backupFileName);
+            await fs.writeFile(outputFileName, Buffer.from(outputFile));
+        }
+    } else {
+        console.error("非対応のファイル形式です")
+        process.exit(1);
+    }
+} catch (error) {
+    console.error(error);
+    process.exit(1);
+}
