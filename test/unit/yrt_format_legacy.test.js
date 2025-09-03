65d0d4731ee4a38309a69fd68457e4f0ae2cddd4
FAL <falworks.contact@gmail.com>
2025-09-03 23:33:14 +0900

    refactor: 未統合ファイルを src 内に移動

--- test/unit/yrt_format_legacy.test.js
+++ test/unit/yrt_format_legacy.test.js
@@ -1,5 +1,5 @@
-import { validateAssetsObject, validateAlreadyMigrated } from "./yrt_format_legacy.mjs";
-import { validateLegacyYrtFormat, validateLegacyLayoutXml } from "./yrt_format_legacy.mjs";
+import { validateAssetsObject, validateAlreadyMigrated } from "../../src/yrt_format_legacy.mjs";
+import { validateLegacyYrtFormat, validateLegacyLayoutXml } from "../../src/yrt_format_legacy.mjs";
 
 describe("validateLegacyLayoutXml", () => {
     describe("基本的な動作", () => {
