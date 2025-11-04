749b376810d95e3de8e01d0c166c729ce7b8528d
FAL <contact@fal-works.com>
2025-11-04 14:32:50 +0900

    fix: 警告の文言を修正

--- src/migrate/warn_foreach_hidden.mjs
+++ src/migrate/warn_foreach_hidden.mjs
@@ -31,6 +31,7 @@ function warnElement(diagnostics, el, originalXml) {
     if (foreach && !isBindingVariable(foreach)) {
         warnWithLocation(diagnostics, originalXml, el, [
             "foreach属性は廃止され、新たにlogic属性が導入されました。",
+            "logic属性ではバインド変数を指定する必要があり、固定値は指定できません。",
             `foreach属性の値 "${foreach}" がバインド変数ではないため、自動変換できません。`,
             "バインド変数を指定してください。"
         ]);
@@ -38,6 +39,7 @@ function warnElement(diagnostics, el, originalXml) {
     if (hidden && !isBindingVariable(hidden)) {
         warnWithLocation(diagnostics, originalXml, el, [
             "hidden属性は廃止され、新たにlogic属性が導入されました。",
+            "logic属性ではバインド変数を指定する必要があり、固定値は指定できません。",
             `hidden属性の値 "${hidden}" がバインド変数ではないため、自動変換できません。`,
             "バインド変数を指定してください。"
         ]);
